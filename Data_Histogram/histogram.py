e54f53e929139183f7246f867144f9be7f0c2806
Yong <kapshaul@gmail.com>
2024-09-16 20:43:10 +0900

    Update plot histogram

--- Data_Histogram/histogram.py
+++ Data_Histogram/histogram.py
@@ -1,19 +1,32 @@
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from collections import Counter
 
-# Define the column to plot histogram
-column_n = 2
-# Define the number of rows to plot histogram
-row_n = 1000000
-# Define the file path
-file_path = './lineitem.tbl'
+# List of items with their index, key name, and associated table
+items = [
+    # Q9
+    #(0, 'ps_partkey', 'partsupp'),
+    #(1, 'l_partkey', 'lineitem'),
+    # Q10
+    #(0, 'c_custkey', 'customer'),
+    #(1, 'o_custkey', 'order'),
+    # Q11 - Modified
+    #(4, 'o_orderdate', 'order'),
+    #(10, 'l_shipdate', 'lineitem'),
+    # Q12
+    (0, 'o_orderkey', 'order'),
+    (0, 'l_orderkey', 'lineitem'),
+    # Q15
+    #(0, 's_suppkey', 'supplier'),
+    #(2, 'l_suppkey', 'lineitem'),
+]
 
-# Load the TBL file with '|' delimiter and no header
-data = pd.read_csv(file_path, delimiter='|', header=None)
-# Select the column data up to the defined number of rows
-column_c = data[column_n][:row_n]
+z_val = ['z0', 'z1', 'z1_5']
+
+# Define the number of rows to plot histogram
+row_n = 100000
 
 # Function to determine if the entire column is numeric or not
 def is_column_numeric(series):
@@ -24,17 +37,42 @@ def is_column_numeric(series):
     except ValueError:
         return False
 
-# Determine if the column is numeric
-if is_column_numeric(column_c):
-    # Convert entire column to numeric
-    hist_data = column_c.astype(float)
-else:
-    # Handle as string data
-    hist_data = column_c
-unique_values = len(set(hist_data))
-plt.hist(hist_data, bins=unique_values, edgecolor='b')
-plt.ylabel('Frequency')
-plt.xlabel('Values')
-plt.title('key')
 
-plt.savefig('key.png')
+# Looping through each item in the 'items' list
+for column, key, table in items:
+
+    # Looping through different distribution
+    for z in z_val:        
+        # Define the file path
+        file_path = '/data/leeyongh-psql/Data/TPCH_10GB/' + z + '/' + table + '.tbl'
+        print("Start, " + key + '_' + z)
+
+        # Load the TBL file with '|' delimiter and no header
+        data = pd.read_csv(file_path, delimiter='|', header=None)
+        # Select the column data up to the defined number of rows
+        column_c = data[column][:row_n]
+
+        # Determine if the column is numeric
+        hist_data = []
+        if is_column_numeric(column_c):
+            # Convert entire column to numeric
+            hist_data = column_c.astype(float)
+            unique_values = np.unique(hist_data)
+        else:
+            # Handle as string data
+            hist_data = column_c
+            unique_values = int(len(set(hist_data)))
+
+        # Create the directory if it doesn't exist
+        output_dir = './' + z
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        plt.figure()
+        plt.hist(hist_data, bins=unique_values, edgecolor='k')
+        plt.ylabel('Frequency')
+        plt.xlabel('Values')
+        plt.title(key + '_' + z)
+
+        plt.savefig('./' + z + '/' + key + '_' + z + '.png')
+        print("Complete, " + key + '_' + z)
