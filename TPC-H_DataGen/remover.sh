1f3f3fdbf274a8ce7dcf18f1bc1afce94c88b524
Yong <kapshaul@gmail.com>
2024-09-27 12:42:19 -0700

    Delete TPC-H_DataGen directory

--- TPC-H_DataGen/remover.sh
+++
@@ -1,8 +0,0 @@
-
-path="/data/leeyongh-psql/Data/TPCH_10GB/z1_5"
-
-tables=("customer" "lineitem" "nation" "order" "part" "partsupp" "region" "supplier")
-
-for table in "${tables[@]}"; do
-    sed -i 's/|$//' "$path/$table.tbl"
-done
